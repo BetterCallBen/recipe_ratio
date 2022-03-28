10c888d04b42fa05152717de6b299bada0da1a59
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-28 22:34:50 +0200

    recipe controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
 User.destroy_all
+puts "cleaning db..."
 
 
 benj = User.create!(
   password: "secret",
   email: "benjbdk@gmail.com"
 )
+
+Recipe.create!(
+  name: "crepe",
+  number_of_people: 5,
+  user: benj,
+)
+
+Recipe.create!(
+  name: "quiche",
+  number_of_people: 4,
+  user: benj,
+)
+
+Recipe.create!(
+  name: "lasagne",
+  number_of_people: 5,
+  user: benj,
+)
+
+puts "#{User.all.count} users were created"
+puts "#{Recipe.all.count} recipes were created"
